8dca32bbc5a70b105830a174c633a8c0282f82e2
Ramon Geronimo <rgero215@gmail.com>
2019-02-28 11:29:53 -0800

    Initial Commit

---
+++ ProductHunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProductHunt
+//
+//  Created by Ramon Geronimo on 2/28/19.
+//  Copyright © 2019 Ramon Geronimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
