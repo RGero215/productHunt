c90e547747b4b9c90d8152cd9843985b521586c4
Ramon Geronimo <rgero215@gmail.com>
2019-02-28 16:19:26 -0800

    Created Post model

---
+++ ProductHunt/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  ProductHunt
+//
+//  Created by Ramon Geronimo on 2/28/19.
+//  Copyright © 2019 Ramon Geronimo. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let name: String
+    let tagline: String
+    let votesCount: Int
+    let commentsCount: Int
+}
